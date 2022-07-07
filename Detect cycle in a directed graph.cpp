02e871a694291540528e294bd2f5f2ef572b913f
sunny jha <78363637+Sunnyjha007@users.noreply.github.com>
2022-07-07 22:41:40 +0530

    Update Detect cycle in a directed graph.cpp

--- Detect cycle in a directed graph.cpp
+++ Detect cycle in a directed graph.cpp
@@ -22,3 +22,27 @@ bool solve(vector<int> adj[],vector<bool>& visited,vector<bool>& visited_dfs,int
         }
         return false;
 }
+//BFS - Kahn's algo
+  bool isCyclic(int n, vector<int> adj[]) {
+        // code here
+        queue<int> pn;
+        vector<int> indegree(n,0);
+        for(int i=0;i<n;i++){
+            for(auto it:adj[i])indegree[it]++;
+        }
+        for(int i=0;i<n;i++){
+	        if(indegree[i]==0)pn.push(i);
+	    }
+	    int cnt=0;
+	    while(!pn.empty()){
+	        int front = pn.front();pn.pop();
+	        cnt++;
+	        for(auto i:adj[front]){
+	            indegree[i]--;
+	            if(indegree[i]==0)pn.push(i);
+	        }
+	    }
+	    if(cnt==n)return false;
+	    return  true;
+        
+    }
